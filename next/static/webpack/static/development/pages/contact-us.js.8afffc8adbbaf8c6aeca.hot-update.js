1fbad7395024bb4c8b34756a75d375b008fd8588
Rafi George <rafihaytham1996@gmail.com>
2019-11-14 23:58:24 -0500

    Updates

---
+++ next/static/webpack/static/development/pages/contact-us.js.8afffc8adbbaf8c6aeca.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/contact-us.js",{
+
+/***/ "./static/svg/arrow-right.svg?include":
+/*!********************************************!*\
+  !*** ./static/svg/arrow-right.svg?include ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<svg viewBox=\"0 0 68 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<path id='arrow-right-1' d=\"M0 7.5L61.878 7.5L67 7.5\" stroke=\"#2F2F2F\" stroke-width=\"2\"></path>\n<path id='arrow-right-2' d=\"M62 1L67 7.5L62 13.5\" stroke=\"#2F2F2F\" stroke-width=\"2\"></path>\n</svg>\n"
+
+/***/ })
+
+})
+//# sourceMappingURL=contact-us.js.8afffc8adbbaf8c6aeca.hot-update.js.map
