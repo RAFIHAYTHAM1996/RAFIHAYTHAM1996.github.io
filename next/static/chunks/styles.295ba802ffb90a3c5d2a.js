9dde4dab4da2c0324860cff83daa8a060a6879b7
Rafi George <rafihaytham1996@gmail.com>
2020-05-14 22:24:23 -0400

    Updates

--- next/static/chunks/styles.295ba802ffb90a3c5d2a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{HY8x:function(n,w,o){}}]);
---
+++ next/static/chunks/styles.7848090756e72095c6ac.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{HY8x:function(n,w,o){}}]);
